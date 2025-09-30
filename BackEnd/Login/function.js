fbfeaaa3f99e9933085d5c05e36afc11f2c92647
JuanCarlos010 <00128023@uca.edu.sv>
2025-09-30 08:33:19 -0600

    correciones hechas al login

--- BackEnd/Login/function.js
+++ BackEnd/Login/function.js
@@ -1,13 +1,19 @@
 function checkPassword() {
+  let name = document.getElementById("User-name");
+  let lastName = document.getElementById("User-lastname");
   let emailAsigned = document.getElementById("User-email");
   let passwordAsigned1 = document.getElementById("User-password");
   let passwordAsigned2 = document.getElementById("Password-confirmation");
-  if (passwordAsigned1.value == "" || passwordAsigned2.value == "") {
-    alert("no puede dejar espacios vacios");
-  } else if (passwordAsigned1.value != passwordAsigned2.value) {
-    alert("Los datos de la contraseña son incorrectos");
-  } else {
-    alert("Datos correctos, redirigiendo...");
+  let validator2 = document.getElementById("validator2");
+  let checkboxInitial = document.getElementById("checkboxInitial");
+  if ((passwordAsigned1.value != passwordAsigned2.value) ||
+      (passwordAsigned1.value == "" && passwordAsigned2.value == "")) {
+    validator2.textContent = "Los datos de la contraseña son diferentes o estan vacios"
+  } else if (passwordAsigned1.value == "" && passwordAsigned2.value == "" &&
+    name.value == "" && lastName.value == "" && emailAsigned.value == "") {
+    validator2.textContent = "No puede dejar espacios vacios";
+  }  else if (!checkboxInitial.checked){ validator2.textContent = "Debe aceptar los terminos y condiciones"} 
+    else {    
     localStorage.setItem("email", emailAsigned.value);
     localStorage.setItem("password", passwordAsigned1.value);
     window.location.href = "Inicio.html";
@@ -17,15 +23,15 @@ function checkPassword() {
 function login() {
   let savedEmail = localStorage.getItem("email");
   let savedPassword = localStorage.getItem("password");
+  let validator = document.getElementById("Validator");
 
   let emailLogin = document.getElementById("emailInput").value.trim();
   let passwordLogin = document.getElementById("passwordInput").value.trim();
 
   if (emailLogin == savedEmail && passwordLogin == savedPassword) {
-    alert("Datos correctos, redirigiendo...");
     window.location.href = "prototipo.html";
   } else {
-    alert("Los datos de la contraseña son incorrectos");
+    validator.textContent="Datos invalidos";
   }
 }
 
