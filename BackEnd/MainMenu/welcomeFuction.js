de574354ed91bd625423de12c53f2250d7730a09
JuanCarlos010 <00128023@uca.edu.sv>
2025-10-29 08:24:41 -0600

    actualizando la script de la base de datos

--- BackEnd/MainMenu/welcomeFuction.js
+++ BackEnd/MainMenu/welcomeFuction.js
@@ -1,7 +1,6 @@
-import {name} from "./Login/function.js"; //Para llamar otros datos del modulo, hay que hacerlo de la forma {x,y,z,...}
-
 function bienvenida(){
     let titulo = document.getElementById("welcomeText");
+    let name = localStorage.getItem("name");
 
     titulo.textContent = "bienvenido " + name;
 }
