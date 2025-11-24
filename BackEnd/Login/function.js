823034fb90da5e17a8096e5aa76cce2d633c6754
JuanCarlos010 <00128023@uca.edu.sv>
2025-11-24 16:52:46 -0600

    prueba

--- BackEnd/Login/function.js
+++ BackEnd/Login/function.js
@@ -45,8 +45,12 @@ async function checkPassword() {
     localStorage.setItem("name", data.name);
     localStorage.setItem("lastName", data.lastName || "");
     localStorage.setItem("email", data.email);
-    
-    window.location.href = "Inicio.html";
+
+    validator2.textContent = "Registro exitoso!";
+    setTimeout(() => {
+      window.location.href = "Inicio.html";
+    }, 1000);
+
   } catch (error) {
     console.log(error.message);
   }
